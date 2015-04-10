0aa1868aa6967df8330ec4a80f7d5989ff7180f8
gwardina <gheawardina@yahoo.com>
2015-04-10 17:08:12 +0700

    Login UI update

--- views/layouts/login.php
+++ views/layouts/login.php
@@ -17,48 +17,27 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
+    
     <title>Login - SIMANPRO PT JAL</title>
     <link rel="shortcut icon" href="http://localhost/propensi/views/layouts/semi-logo.png" />
+    <link href="<?php echo Yii::$app->params['base']?>css/sb-admin-2.css" rel="stylesheet">
+    
     <?php $this->head() ?>
 </head>
 <body>
 
 <?php $this->beginBody() ?>
-    <div class="wrap">
-        <?php
-            NavBar::begin([
-                'brandLabel' => 'SIMANPRO',
-                'brandUrl' => Yii::$app->homeUrl,
-                'options' => [
-                    'class' => 'navbar-default navbar-fixed-top',
-                ],
-            ]);
-            echo Nav::widget([
-                'options' => ['class' => 'navbar-nav navbar-right'],
-                'items' => [
-                    Yii::$app->user->isGuest ?
-                        ['label' => 'Login', 'url' => ['/login']] :
-                        ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                            'url' => ['/login/logout'],
-                            'linkOptions' => ['data-method' => 'post']],
-                ],
-            ]);
-            NavBar::end();
-        ?>
-
-        <div class="container">
-            <?= Breadcrumbs::widget([
-                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-            ]) ?>
-            <?= $content ?>
-        </div>
+    <div class="line" style="top: 0px; height: 50px;"></div>
+    
+    <center><img class="img-logo-login" src="http://localhost/propensi/views/layouts/logo.png"></center>
+    
+    <div class="login-box" style="margin-top: 10px; padding-bottom: 3px;">
+    	<?= $content ?>
     </div>
+  
 
-    <footer class="footer">
-        <div class="container">
-            <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-            <p class="pull-right"><?= Yii::powered() ?></p>
-        </div>
+    <footer class="line">
+        <p class="pull-left">&copy; PT Jaya Anugrah Lestari <?= date('Y') ?></p>
     </footer>
 
 <?php $this->endBody() ?>
--- views/login/login.php
+++ views/login/login.php
@@ -9,40 +9,26 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<center>
-<div class="site-login" style="margin: 0px auto;">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out the following fields to login:</p>
-
     <?php $form = ActiveForm::begin([
         'id' => 'login-form',
         'options' => ['class' => 'form-horizontal'],
         'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-            'labelOptions' => ['class' => 'col-lg-1 control-label'],
+            'template' => "\n<div class=\"input-box\">{input}</div>",
         ],
     ]); ?>
 
-    <?= $form->field($model, 'username') ?>
+    <?= $form->field($model, 'username')->textInput(['placeholder' => 'Username']) ?>
 
-    <?= $form->field($model, 'password')->passwordInput() ?>
+    <?= $form->field($model, 'password')->passwordInput(['placeholder' => 'Password']) ?>
     
-    <?= $form->field($model, 'rememberMe', [
-        'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-    ])->checkbox() ?>
-
-    <div class="form-group">
-        <div class="col-lg-offset-1 col-lg-11">
-            <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+        <div class="form-group">
+        <div class="col-lg-12">
+            <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button', 'style' => 'width:100%;']) ?>
         </div>
     </div>
 
     <?php ActiveForm::end(); ?>
 
-    <div class="col-lg-offset-1" style="color:#999;">
-        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
-    </div>
+  
 </div>
 </center>
