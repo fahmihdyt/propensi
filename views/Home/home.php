77e940793ddffce4f344259db4fc14c52f7e2782
gwardina <gheawardina@yahoo.com>
2015-05-10 22:44:02 +0700

    Home!
    -alhamdulillah home udah jadi wallop pas2an :))))
    -ngubah warna dikit di sb2 admin :)

--- views/Home/home.php
+++ views/Home/home.php
@@ -1,5 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use app\models\Pengumuman;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\ProjectSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Welcome to Sistem Manajemen Proyek PT JAL';
+$this->params['breadcrumbs'][] = $this->title;
+$data = Pengumuman::find()->orderBy(['tanggal' => 'ASC'])->all();
+?>
+<head>
+	<link href="<?php echo Yii::$app->params['base']?>css/sb2-admin.css" rel="stylesheet">
+</head>
+
+<body>
 <center>
-	<h1 style='padding-top:25px; margin-top:0px;'>Welcome to Sistem Manajemen Proyek PT JAL</h1>
-	<hr>
-	<h3>You are now logged in as <?php echo Yii::$app->user->identity->jabatan ?></h3>
+	<div style="margin-top: 0px; padding-top: 10px;">
+    <h1><?= Html::encode($this->title) ?></h1>
+    <hr></div><br>
 </center>
+
+<?php foreach ($data as $line) { ?>
+	<div class="panel panel-red">
+		<div class="panel-heading">
+			<?= $line->judul; ?>
+		</div>
+		
+		<div class="panel-body">
+			<?= $line->isi; ?>
+		</div>
+		
+		<div class="panel-footer">
+			Posted by:&nbsp;<?= $line->getCreator($line->creator) ?>&nbsp;on&nbsp;<?= $line->tanggal ?>
+		</div>
+	</div>
+<?php } ?>
+</body>
